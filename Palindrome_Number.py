9c26bc24577a4f0ae77c1e7bc1d65b7f8b8a9b1f
arvis <arvis@180814-MAC-ITSD-2.local>
2020-02-18 17:56:56 +0800

    fix indent

--- Palindrome_Number.py
+++ Palindrome_Number.py
@@ -1,19 +1,19 @@
 class Solution(object):
-	# https://leetcode.com/problems/palindrome-number/
+  # https://leetcode.com/problems/palindrome-number/
     def isPalindrome(self, x):
         """
         :type x: int
         :rtype: bool
         """
         if x < 0 :
-        	return False
+          return False
 
         original_x = x
         reverse = 0
 
         while x > 0:
-        	reverse = reverse * 10 + x % 10
-        	x //= 10
+          reverse = reverse * 10 + x % 10
+          x //= 10
 
         return reverse == original_x
 
@@ -22,11 +22,11 @@ class Solution(object):
 
 
 if __name__ == '__main__':
-	s = Solution()
-	print(s.isPalindrome(121))
+  s = Solution()
+  print(s.isPalindrome(121))
 
-	print(s.isPalindrome(-121))
+  print(s.isPalindrome(-121))
 
-	print(s.isPalindrome(2121))
+  print(s.isPalindrome(2121))
 
-	print(s.isPalindrome(23442))
+  print(s.isPalindrome(23442))
--- number_of_steps.py
+++ number_of_steps.py
@@ -1,6 +1,6 @@
 
 class Solution(object):
-	# https://leetcode.com/problems/number-of-steps-to-reduce-a-number-to-zero/
+  # https://leetcode.com/problems/number-of-steps-to-reduce-a-number-to-zero/
   def numberOfSteps(self, num, times=0):
       """
       :type num: int
@@ -8,20 +8,20 @@ class Solution(object):
       """
 
       if num == 0:
-      	return times
+        return times
 
       if num % 2 != 0:
-      	return self.numberOfSteps(num-1, times+1)
+        return self.numberOfSteps(num-1, times+1)
       else:
-      	return self.numberOfSteps(num//2, times+1)
+        return self.numberOfSteps(num//2, times+1)
    
 
 
 if __name__ == '__main__':
-	s = Solution()
-	print(s.numberOfSteps(14))
+  s = Solution()
+  print(s.numberOfSteps(14))
 
-	print(s.numberOfSteps(8))
+  print(s.numberOfSteps(8))
 
-	print(s.numberOfSteps(123))
+  print(s.numberOfSteps(123))
 
